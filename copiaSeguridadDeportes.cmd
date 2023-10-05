d3152b7c0f3d5106719028babbd950fe7c51d0dc
Mauro Asselborn <mauasselborn@gmail.com>
2023-10-05 00:28:02 -0300

    FIN DEL PROGRAMA

--- copiaSeguridadDeportes.cmd
+++
@@ -1,5 +0,0 @@
-@echo off
-
-if exist files\Deportes.dat del files\Deportes.dat
-
-copy files\BackUps\DEPORTES.BKP files\Deportes.dat
--- copiaSeguridadJugadores.cmd
+++
@@ -1,6 +0,0 @@
-@echo off
-
-if exist files\Jugadores.dat del files\Jugadores.dat
-
-copy files\BackUps\JUGADORES.BKP files\Jugadores.dat
-
